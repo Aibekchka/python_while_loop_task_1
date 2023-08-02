a126b4916b2357b2c7a3efee69478fab583f85eb
Aibekchka <140369809+Aibekchka@users.noreply.github.com>
2023-08-02 11:45:56 +0600

    Add files via upload

---
+++ while_loop_task_1.py
@@ -0,0 +1,19 @@
+print('1. Пистолет')
+print('2. Автомат')
+print('3. РПГ')
+print('0. Выйти')
+weapon =int(input('Выберите оружие: '))
+while weapon!=0:
+    if weapon == 1:
+        print('пиу пиу')
+    elif weapon == 2:
+        print('дрт дрт')
+    elif weapon == 3:
+        print('Boom Boom!')
+    print('1. Пистолет')
+    print('2. Автомат')
+    print('3. РПГ')
+    print('0. Выйти')
+    weapon = int(input('Выберите оружие: '))
+
+print('Вы Вышли')
